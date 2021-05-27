c7bf8f77f8b56b0c0de8556ca540a169a0862e54
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-05-26 21:26:30 -0400

    refactor files

--- roles/app_access_gateway/files/cronjobs_access.sh
+++ roles/app_access_gateway/files/cronjobs_access.sh
@@ -7,6 +7,7 @@ echo "Executing cron jobs at $(date)"
 certbot renew
 
 for host in $HOSTS; do
+	echo "---> $host"
 	scp /etc/letsencrypt/live/tinynet.chrisx.xyz/{fullchain,privkey}.pem "root@$host:/etc/ssl/private/"
 done
 
