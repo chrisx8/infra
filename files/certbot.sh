011f3727661371f01f4c0bd198867378654f6e02
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2023-07-22 12:25:37 -0500

    indent with spaces

--- files/certbot.sh
+++ files/certbot.sh
@@ -13,95 +13,95 @@ OP="$2"
 ################################################################################
 
 function fail() {
-	echo "Usage: certbot.sh WORKDIR certbot-(clean|issue|list|renew|setup)" 1>&2
-	exit 1
+    echo "Usage: certbot.sh WORKDIR certbot-(clean|issue|list|renew|setup)" 1>&2
+    exit 1
 }
 
 function setup() {
-	echo "#####################################################################"
-	echo "    chrisx8/infra certbot utilities"
-	echo "#####################################################################"
-	echo
-	echo "Creating certbot environment..."
-	python3 -m venv "$WORKDIR/venv"
-	# shellcheck source=/dev/null
-	source "$WORKDIR/venv/bin/activate"
-	echo
-	echo "Installing certbot..."
-	pip install -U pip wheel
-	pip install -U certbot-dns-cloudflare
-	echo
+    echo "#####################################################################"
+    echo "    chrisx8/infra certbot utilities"
+    echo "#####################################################################"
+    echo
+    echo "Creating certbot environment..."
+    python3 -m venv "$WORKDIR/venv"
+    # shellcheck source=/dev/null
+    source "$WORKDIR/venv/bin/activate"
+    echo
+    echo "Installing certbot..."
+    pip install -U pip wheel
+    pip install -U certbot-dns-cloudflare
+    echo
 }
 
 function clean() {
-	rm -rf "$WORKDIR/venv"
+    rm -rf "$WORKDIR/venv"
 }
 
 function issue() {
     setup
-	DOMAINS=""
-	echo "Loading config from $WORKDIR/env"
-	# shellcheck source=/dev/null
-	source "$WORKDIR/env"
-	echo "Issuing certs with certbot..."
-	for DOMAIN in $DOMAINS; do
-		certbot certonly \
-			--config-dir "$WORKDIR/config" \
-			--logs-dir "$WORKDIR/log" \
-			--work-dir "$WORKDIR/workdir" \
-			--no-eff-email --email "$EMAIL" \
-			-d "$DOMAIN" \
-			--dns-cloudflare \
-			--dns-cloudflare-credentials "$WORKDIR/cloudflare.ini"
-	done
+    DOMAINS=""
+    echo "Loading config from $WORKDIR/env"
+    # shellcheck source=/dev/null
+    source "$WORKDIR/env"
+    echo "Issuing certs with certbot..."
+    for DOMAIN in $DOMAINS; do
+        certbot certonly \
+            --config-dir "$WORKDIR/config" \
+            --logs-dir "$WORKDIR/log" \
+            --work-dir "$WORKDIR/workdir" \
+            --no-eff-email --email "$EMAIL" \
+            -d "$DOMAIN" \
+            --dns-cloudflare \
+            --dns-cloudflare-credentials "$WORKDIR/cloudflare.ini"
+    done
 }
 
 function renew() {
     setup
-	echo "Renewing certs with certbot..."
-	certbot renew \
-		--config-dir "$WORKDIR/config" \
-		--logs-dir "$WORKDIR/log" \
-		--work-dir "$WORKDIR/workdir"
+    echo "Renewing certs with certbot..."
+    certbot renew \
+        --config-dir "$WORKDIR/config" \
+        --logs-dir "$WORKDIR/log" \
+        --work-dir "$WORKDIR/workdir"
 }
 
 function list() {
     setup
-	certbot certificates \
-		--config-dir "$WORKDIR/config" \
-		--logs-dir "$WORKDIR/log" \
-		--work-dir "$WORKDIR/workdir"
+    certbot certificates \
+        --config-dir "$WORKDIR/config" \
+        --logs-dir "$WORKDIR/log" \
+        --work-dir "$WORKDIR/workdir"
 }
 
 [ "$#" != "2" ] && fail
 
 if [ ! -d "$WORKDIR" ]; then
-	echo "$WORKDIR is not a valid directory."
-	read -r -p "Create? (y/n) " create_dir
-	if [ "$create_dir" == "y" ]; then
-		mkdir -p "$WORKDIR"
-	else
-		exit 1
-	fi
+    echo "$WORKDIR is not a valid directory."
+    read -r -p "Create? (y/n) " create_dir
+    if [ "$create_dir" == "y" ]; then
+        mkdir -p "$WORKDIR"
+    else
+        exit 1
+    fi
 fi
 
 case "$OP" in
-	"certbot-clean")
-		clean
-		;;
-	"certbot-issue")
-		issue
-		;;
+    "certbot-clean")
+        clean
+        ;;
+    "certbot-issue")
+        issue
+        ;;
     "certbot-list")
         list
         ;;
-	"certbot-renew")
-		renew
-		;;
-	"certbot-setup")
-		setup
-		;;
-	*)
-		fail
-		;;
+    "certbot-renew")
+        renew
+        ;;
+    "certbot-setup")
+        setup
+        ;;
+    *)
+        fail
+        ;;
 esac
--- roles/containers_web/templates/backup.sh
+++ roles/containers_web/templates/backup.sh
@@ -7,10 +7,10 @@ curl -fsS -m 3 "{{ containers_web_cron_ping_url }}/start?rid=$RID"
 echo "[Healthchecks start]"
 
 for v in $(podman volume ls -q); do
-	echo "Backing up $v..."
-	pushd "$HOME/.local/share/containers/storage/volumes/$v" > /dev/null
-	podman unshare tar czvf "/tmp/$v.tgz" "_data/"
-	popd > /dev/null
+    echo "Backing up $v..."
+    pushd "$HOME/.local/share/containers/storage/volumes/$v" > /dev/null
+    podman unshare tar czvf "/tmp/$v.tgz" "_data/"
+    popd > /dev/null
 done
 
 echo "====== List of backups ======"
