6da6160eba6f22f52be8b3a31681518e0f55d936
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2024-03-21 21:03:09 -0500

    remove make, add virtualenv helper scripts
    * Drop Makefile
    * Add virtualenv helper scripts, for setting up shell aliases (replacing
      `make` targets) and bootstraping/updating environment
    * prep for switch to `uv`

---
+++ utils/envsetup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+export PIPENV_VENV_IN_PROJECT=1
+
+if [ ! -f "requirements".txt ] || [ ! -f "requirements.yml" ]; then
+  echo "Please run this script in the repo's base directory." >&2
+  exit 1
+fi
+
+echo "*********************************************************************"
+echo " Bootstrapping Ansible virtualenv"
+echo "*********************************************************************"
+
+if [ -d .venv ]; then
+  echo "## Using existing virtualenv at .venv"
+else
+  echo "## Creating virtualenv at .venv"
+  uv venv
+fi
+
+echo "## Installing Python packages with pipenv"
+pipenv install --dev
+# uv pip install -r requirements.txt
+source .venv/bin/activate
+
+echo "## Installing Ansible collections"
+ansible-galaxy collection install -U -r requirements.yml
+
+echo "## Installing pre-commit hooks"
+pre-commit install --install-hooks
---
+++ utils/envupdate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [ ! -d "$INFRA_REPO_DIR" ]; then
+  echo "Please run \`source ./activate\` before executing this script." >&2
+  exit 1
+fi
+
+echo "*********************************************************************"
+echo " Updating Ansible virtualenv"
+echo "*********************************************************************"
+
+pushd "$INFRA_REPO_DIR" > /dev/null
+# echo "## Compiling Python package list (requirements.txt)"
+# uv pip compile requirements.in > requirements.txt
+echo "## Updating Python packages with pipenv"
+pipenv update --dev
+echo "## Updating Ansible collections"
+ansible-galaxy collection install -U -r requirements.yml
+echo "## Updating pre-commit hooks"
+pre-commit autoupdate
+popd > /dev/null
